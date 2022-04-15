eaeb2dbe7fc1e7e0c8ac3999e5cda57f7fb5cdae
Touko-g <chenguanxi70@gmail.com>
2022-04-15 18:06:53 +0800

    requestAnimationFrame

---
+++ jsdoc/index.js
@@ -0,0 +1,19 @@
+// let count = 0
+// let anima = null
+//
+// function show(max) {
+//     el.innerHTML = count.toString()
+//     count++
+//     anima = requestAnimationFrame(() => {
+//         show(max)
+//     })
+//     if (count >= max) {
+//         cancelAnimationFrame(anima)
+//     }
+// }
+// show(200)
+//
+// function namespace(obj,str) {
+//
+// }
+
--- requestAnimationFrame/app.js
+++ requestAnimationFrame/app.js
@@ -1,23 +1,44 @@
-const el = document.querySelector('#num')
+const el = document.querySelectorAll('.num')
 
-const loopFactory = function (func) {
-    let i = 0
-    let handle = {}
-    let loop = function () {
-        i = func(i)
-        handle.id = requestAnimationFrame(loop)
+let num = 0
+let animaObj = {}
+
+const loopMore = () => {
+    Array.from(el).forEach((v, i) => {
+        const l = () => {
+            v.innerHTML = new Date().getTime()
+            num++
+            animaObj['num' + i] = requestAnimationFrame(l)
+        }
+        l()
+    })
+}
+loopMore()
+
+Array.from(el).forEach((v, i) => {
+    v.onclick = function () {
+        cancelAnimationFrame(animaObj['num' + i])
     }
-    handle.id = requestAnimationFrame(loop);
-    return handle
-}
+})
 
-let increment = function (i) {
-    el.innerHTML = i
-    return ++i
-}
-
-let interval = loopFactory(increment)
-
-window.onclick = function () {
-    cancelAnimationFrame(interval.id)
-}
+// const loopFactory = function (func) {
+//     let i = 0
+//     let handle = {}
+//     let loop = function () {
+//         i = func(i)
+//         handle.id = requestAnimationFrame(loop)
+//     }
+//     handle.id = requestAnimationFrame(loop);
+//     return handle
+// }
+//
+// let increment = function (i) {
+//     el[0].innerHTML = i
+//     return ++i
+// }
+//
+// let interval = loopFactory(increment)
+//
+// window.onclick = function () {
+//     cancelAnimationFrame(interval.id)
+// }
